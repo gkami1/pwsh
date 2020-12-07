9b343d2797f737e027d0de44a2072b0cc795e3aa
gkami1 <kgabdulkhakov2017@litsey2.ru>
2020-12-07 22:52:06 +0300

    1

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from random import randint
+import requests
+
+
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/haba')
+
+@app.route('/haba/')
 def hello_world():
 
     s = ["Hello, Haba!",
@@ -11,3 +15,9 @@ def hello_world():
 
     out = "<pre>{}</pre>".format("\n".join(s))
     return out
+
+
+@app.route('/task1/random/')
+def rand():
+    return "Haba's mark is {}".format(str(randint(1, 5)))
+
