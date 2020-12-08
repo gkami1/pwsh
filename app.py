7cd9e3164ffaf787e1fd57b541a21949b4cfd687
gkami1 <kgabdulkhakov2017@litsey2.ru>
2020-12-08 22:59:41 +0300

    7

--- app.py
+++ app.py
@@ -5,21 +5,22 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+
 @app.route('/haba/')
 def hello_world():
 
-    s = ["Hello, Haba!",
-        "Hello, Arsen!",
-        "Hello, Karim!"]
+s = ["Hello, Haba!",
+"Hello, Arsen!",
+"Hello, Karim!"]
 
-    out = "<pre>{}</pre>".format("\n".join(s))
-    return out
+out = "<pre>{}</pre>".format("\n".join(s))
+return out
 
 
 @app.route('/task1/random/')
 def rand():
-    return "Haba's mark is {}".format(str(randint(1, 5)))
+return "Haba's mark is {}".format(str(randint(1, 5)))
+
 
 @app.route('/task1/i_will_not/')
 def i_will_not():
@@ -27,4 +28,13 @@ ans = ['<ul type="disc" id="blackboard">']
 for i in range(100):
 ans.append('<li>I will not waste time</li>')
 ans.append('</ul>')
+return "<pre>{}</pre>".format(''.join(ans))
+
+
+@app.route('/')
+def menu():
+ans = ['<ul type="disc" id="menu">',
+'<li><a href="/task1/random/">/task1/random/</a></li>',
+'<li><a href="/task1/i_will_not/">/task1/i_will_not/</a></li>',
+'</ul>']
 return "<pre>{}</pre>".format(''.join(ans))
