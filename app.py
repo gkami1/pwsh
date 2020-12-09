0603cbc2e3514dc9e56612ac6fc43cb21a78393a
gkami1 <kgabdulkhakov2017@litsey2.ru>
2020-12-09 23:21:15 +0300

    1

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
-from flask import Flask
 from random import randint
 import requests
-
+import inflect
+import json
 
 app = Flask(__name__)
-
-
-@app.route('/haba/')
-def hello_world():
-
-    s = ["Hello, Haba!",
-        "Hello, Arsen!",
-        "Hello, Karim!"]
-
-    out = "<pre>{}</pre>".format("\n".join(s))
-    return out
-
 
 @app.route('/task1/random/')
 def rand():
     return "Haba's mark is {}".format(str(randint(1, 5)))
-
-
 @app.route('/task1/i_will_not/')
 def i_will_not():
     ans = ['<ul type="disc" id="blackboard">']
@@ -29,8 +15,6 @@ def i_will_not():
         ans.append('<li>I will not waste time</li>')
     ans.append('</ul>')
     return "<pre>{}</pre>".format(''.join(ans))
-
-
 @app.route('/')
 def menu():
     ans = ['<ul type="disc" id="menu">',
@@ -39,3 +23,36 @@ def menu():
            '</ul>']
     return "<pre>{}</pre>".format(''.join(ans))
 
+@app.route('/task2/avito/<city>/<category>/<ad>/')
+def avito(city, category, ad):
+    out = """<h1>debug info</h1><p>city={} category={} ad={}</p><h1>{}</h1><p>{}</p>""".format(city, category, ad, category[1], city[1])
+    return out
+
+@app.route('/task2/cf/profile/<username>')
+def codeforces(username):
+    url = 'https://codeforces.com/api/user.info?handles={}'.format(username)
+    ans = '<table id="stats" border="1"><tr>{}</tr><tr>{}</tr></table>'
+    f = '<td>User</td><td>Rating</td>'
+    s = '<td>{}</td><td>{}</td>'
+    req = requests.get(url)
+    k = req.json()
+    if k['status'] != 'OK':
+        return 'User not found'
+    return ans.format(f, s.format(username, str(k['result'][0]['rating'])))
+
+@app.route('/task2/num2words/<num>/')
+def numc(num):
+    if int(num) < 0 or int(num) > 999:
+        return json.dumps({"status": "FAIL"})
+    else:
+        p = inflect.engine()
+        l = p.number_to_words(int(num))
+        if 'and' in l:
+            l = ''.join(l.split(' and'))
+        if '-' in l:
+            l = ' '.join(l.split('-'))
+        if int(num) % 2 == 0:
+            m = True
+        else:
+            m = False
+        return json.dumps({"status": "OK", "number": int(num), "isEven": m, "words": str(l)})
